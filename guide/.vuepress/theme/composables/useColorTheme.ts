20a8718a0791f5d887bb09e359a537177bc50160
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-08-16 23:10:17 -0400

    Update useColorTheme.ts

--- guide/.vuepress/theme/composables/useColorTheme.ts
+++ guide/.vuepress/theme/composables/useColorTheme.ts
@@ -1,19 +1,19 @@
 export default function useColorTheme() {
-	const setTheme = ({ colorTheme = 'default', persist = false }) => {
-		const themes = ['blurple'];
+	const setTheme = ({ colorTheme = 'blurple', persist = false }) => {
+		const themes = ['default'];
 		const { classList } = document.documentElement;
 		const themesClasses = themes.map(theme => `guide-theme-${theme}`);
 
-		if (colorTheme !== 'default' && !themes.includes(colorTheme)) {
+		if (colorTheme !== 'blurple' && !themes.includes(colorTheme)) {
 			const oldTheme = localStorage.getItem('guide-color-theme');
-			colorTheme = themes.includes(oldTheme) ? oldTheme : 'default';
+			colorTheme = themes.includes(oldTheme) ? oldTheme : 'blurple';
 		}
 
 		if (persist) {
 			localStorage.setItem('guide-color-theme', colorTheme);
 		}
 
-		if (colorTheme === 'default') return classList.remove(...themesClasses);
+		if (colorTheme === 'blurple') return classList.remove(...themesClasses);
 		classList.remove(...themesClasses.filter(themeClass => themeClass !== `guide-theme-${colorTheme}`));
 		classList.add(`guide-theme-${colorTheme}`);
 	};
